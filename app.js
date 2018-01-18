8b11a2254c3e014aa41af843addb8ce06cda6629
Meir Blachman <meirblachman@protonmail.com>
2018-01-18 23:18:40 +0200

    more package-locks

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const rp = require("request-promise");
 const { writeFileSync, mkdirSync, existsSync } = require("fs");
-const { exec } = require("child_process");
+const { exec, execSync } = require("child_process");
 const rimraf = require("rimraf");
 
 const { npmModules, earliestVersion } = require("./config");
@@ -17,18 +17,18 @@ const { npmModules, earliestVersion } = require("./config");
 
     for (const version in body.time) {
       if (["modified", "created"].includes(version)) continue;
-      if(version.toUpperCase() !== version) continue;
+      if (version.toUpperCase() !== version) continue;
       if (new Date(body.time[version]) < earliestVersion) continue;
-      console.log(`creating directory`, npmModule, version, body.time[version]);
-      createDirectoryForVersion(npmModule, version);
+      createDirectoryForVersion(npmModule, version, body.time[version]);
     }
   }
 })();
 
-function createDirectoryForVersion(name, version) {
+function createDirectoryForVersion(name, version, time) {
   const directory = `./packages/${name}@${version}`;
   if (!existsSync(directory)) {
     mkdirSync(directory);
+    console.log(`creating directory`, name, version, time);
   }
   const packageJson = {
     dependencies: {
@@ -44,15 +44,30 @@ function createDirectoryForVersion(name, version) {
     `${directory}/package.json`,
     JSON.stringify(packageJson, null, 2)
   );
-  exec("npm install", { cwd: directory }, (error, sdtout, sdterr) => {
-    if (error) {
-      console.error(
-        `failed to install dependencies for ${name}@${version}`,
-        error
-      );
-      return;
+
+  npmInstall(true);
+
+  function npmInstall(sync) {
+    if (sync) {
+      try {
+        execSync("npm install", { cwd: directory });
+        console.info(`installed dependencies for ${name}@${version}`);
+        rimraf.sync(`${directory}/node_modules`);
+      } catch (error) {
+        console.error(`failed to install dependencies for ${name}@${version}`);
+      }
+    } else {
+      exec("npm install", { cwd: directory }, (error, sdtout, sdterr) => {
+        if (error) {
+          console.error(
+            `failed to install dependencies for ${name}@${version}`,
+            error
+          );
+          return;
+        }
+        console.info(`installed dependencies for ${name}@${version}`);
+        rimraf.sync(`${directory}/node_modules`);
+      });
     }
-    console.info(`installed dependencies for ${name}@${version}`);
-    rimraf.sync(`${directory}/node_modules`);
-  });
+  }
 }
--- config.js
+++ config.js
@@ -5,7 +5,6 @@ module.exports = {
     "request",
     "chalk",
     "async",
-    "express",
     "bluebird",
     "commander",
     "react",
